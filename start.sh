9da94ce6031c27cb7741352f4786f397eb76f2c4
ROYAL-GAGAN-786 <133026312+ROYAL-GAGAN-786@users.noreply.github.com>
2023-05-09 21:06:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
+clone https://github.com/ROYAL-GAGAN-786/Mdisk-Search-Botgit /MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
