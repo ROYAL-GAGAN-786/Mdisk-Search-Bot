30bbdf4d0aac31608eec8300a03c08fa41944aa1
ROYAL-GAGAN-786 <133026312+ROYAL-GAGAN-786@users.noreply.github.com>
2023-06-06 00:39:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", "25830285"))
     API_HASH = os.getenv("API_HASH", "0cbcabbf1b9c56a4dfc4b8950d584a2a")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5790577269:AAFmFot7U94XFfD7V43fJAbrRpQBLUQ0EXg")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "6157260907:AAEd3guBgQBaTJO03YCO1BJvs3swdMRjfqc")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOJMBu2J2m9ajUYQuuftJ8ohqBO-dYZnywqaUuU5dau19X7n4CMM23icbY404Q7GNPkvQuEcjCEMhKHB_jHaZWlK1Cif9vKlVugawdZKyesDi788UHIimsFx0nSSa_9KHeq9Lpby-nG-74j4ZmpCsMcfmYPMmp5BUxXrKZI4bbAjJwxdAJvJbw3goGpZkYNfvwqBEWgDYiQAbRKHblK8axmz9S8pnAh2T3dbURzSF5O9BrR292janWioYKbsUzdItEIs3EITuYDpQltvaDIvigzLHZvPMy4pXKXTRyA0yBTxsXkBD8qlvpYl-_qw3VOm-dEPuo_fTGMi5ch4CxlmbULvBYaI=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001902573454"))
